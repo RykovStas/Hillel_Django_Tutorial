b8ba4c9b98a4b89d9775a4f406f4735bcb7c62b1
Stas <stasyan_2003@icloud.com>
2023-03-04 14:12:22 +0200

    Django App Part 7 - Admin Panel

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +10,13 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+
 
     def was_published_recently(self):
         now = timezone.now()
